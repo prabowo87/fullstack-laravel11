2d72d4b097a2a926fbf433d0e43d371e10ea7d7a
Hermawan Prabowo <mboh.94er0h@gmail.com>
2024-05-20 06:56:50 +0700

    fix migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,12 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('photo')->nullable();
-            $table->string('birth_date')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
-            $table->string('profession')->nullable();
-            $table->enum('is_complete',['YES', 'NOT'])->default('NOT');
             $table->timestamps();
         });
         
